8550e1990410ea2ea6b321b3e5939713b0770920
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-07-21 17:50:52 +0400

    add language change sounds

--- src/components/Navbar/LanguageChanger.tsx
+++ src/components/Navbar/LanguageChanger.tsx
@@ -2,48 +2,59 @@
 import classNames from "classnames";
 import { useRouter } from "next/navigation";
 import { usePathname } from "next/navigation";
+import useSoundStore from "@/store/use-sound-store";
+import useSound from "use-sound";
+import toEN from "@/sounds/toEnglish.mp3";
+import toKA from "@/sounds/toGeorgian.mp3";
 
 export default function LanguageChanger({ locale }: { locale: string }) {
-    let currentLocale = locale;
-    const router = useRouter();
-    const currentPathname = usePathname();
-    const handleChange = (newLocale: string) => {
-        if (typeof window !== "undefined") {
-            window.localStorage.setItem("NEXT_LOCALE", newLocale);
-        }
-        router.push(
-            currentPathname.replace(`/${currentLocale}`, `/${newLocale}`)
-        );
-        router.refresh();
-    };
+  let currentLocale = locale;
+  const router = useRouter();
+  const currentPathname = usePathname();
+  const { sound } = useSoundStore();
+  const [toEnglish] = useSound(toEN);
+  const [toGeorgian] = useSound(toKA);
+  const handleChange = (newLocale: string) => {
+    if (typeof window !== "undefined") {
+      window.localStorage.setItem("NEXT_LOCALE", newLocale);
+    }
+    router.push(currentPathname.replace(`/${currentLocale}`, `/${newLocale}`));
+    router.refresh();
+  };
 
-    return (
-        <div className="flex gap-[8px] items-center">
-            <button
-                onClick={() => handleChange("en")}
-                className="p-[8px] cursor-pointer text-primary dark:text-secondary text-[18px] font-bold font-graphik"
-            >
-                EN
-            </button>
-            <div className="w-[48px] relative h-[2px]">
-                <div className="relative w-full h-full bg-primary dark:bg-secondary dark:bg-opacity-[30%] bg-opacity-[30%]"></div>
-                <div
-                    className={classNames(
-                        "absolute w-[50%] top-0 h-full bg-primary dark:bg-secondary",
-                        {
-                            "right-0": currentLocale === "ka",
-                            "left-0": currentLocale === "en",
-                        }
-                    )}
-                ></div>
-            </div>
+  return (
+    <div className="flex gap-[8px] items-center">
+      <button
+        onClick={() => {
+          handleChange("en");
+          sound && toEnglish();
+        }}
+        className="p-[8px] cursor-pointer text-primary dark:text-secondary text-[18px] font-bold font-graphik"
+      >
+        EN
+      </button>
+      <div className="w-[48px] relative h-[2px]">
+        <div className="relative w-full h-full bg-primary dark:bg-secondary dark:bg-opacity-[30%] bg-opacity-[30%]"></div>
+        <div
+          className={classNames(
+            "absolute w-[50%] top-0 h-full bg-primary dark:bg-secondary",
+            {
+              "right-0": currentLocale === "ka",
+              "left-0": currentLocale === "en",
+            }
+          )}
+        ></div>
+      </div>
 
-            <button
-                onClick={() => handleChange("ka")}
-                className="p-[8px] cursor-pointer text-primary dark:text-secondary text-[18px] font-bold font-graphik"
-            >
-                KA
-            </button>
-        </div>
-    );
+      <button
+        onClick={() => {
+          handleChange("ka");
+          sound && toGeorgian();
+        }}
+        className="p-[8px] cursor-pointer text-primary dark:text-secondary text-[18px] font-bold font-graphik"
+      >
+        KA
+      </button>
+    </div>
+  );
 }
