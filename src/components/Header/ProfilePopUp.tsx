80792d11b6e5dcd55173710bdee5b06950929e8e
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-08-14 21:56:08 +0400

    add core components

--- src/components/Header/ProfilePopUp.tsx
+++ src/components/Header/ProfilePopUp.tsx
@@ -7,6 +7,7 @@ import React from "react";
 
 export default function ProfilePopUp() {
   const { data, loading, error } = useQuery<IProfileResponse>(GET_PROFILE);
+  console.log(data);
   const { isOpen, type } = usePopUpStore();
   return (
     <div
