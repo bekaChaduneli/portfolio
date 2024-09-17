512400f0f95192469ee9a1e74baade2afbe5f931
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-09-17 23:42:08 +0400

    add live chat styles

--- src/app/[locale]/template.tsx
+++ src/app/[locale]/template.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 export default function Template({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     animatePageIn();
+
     (function (d: Document, t: string) {
       const BASE_URL = "https://app.chatwoot.com";
       const g = d.createElement(t) as HTMLScriptElement;
@@ -22,18 +23,26 @@ export default function Template({ children }: { children: React.ReactNode }) {
           websiteToken: "njrbmeGxbymze4dVorUbp6Hb",
           baseUrl: BASE_URL,
         });
+
+        const chatButton = document.querySelector(
+          ".chatwoot-widget"
+        ) as HTMLElement;
+        if (chatButton) {
+          chatButton.style.zIndex = "1";
+        }
       };
     })(document, "script");
   }, []);
+
   return (
-    <div>
+    <div className="relative">
       <div
         id="banner-1"
         className="min-h-screen bg-primary dark:bg-[#131c3e] z-[60] fixed top-0 left-0 w-[20%]"
       />
       <div
         id="banner-2"
-        className=" min-h-screen bg-primary dark:bg-[#131c3e] z-[60] fixed top-0 left-[20%] w-[20%]"
+        className="min-h-screen bg-primary dark:bg-[#131c3e] z-[60] fixed top-0 left-[20%] w-[20%]"
       />
       <div
         id="banner-3"
